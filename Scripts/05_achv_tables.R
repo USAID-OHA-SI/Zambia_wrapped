3900a4ea950c09ec191625dee24e9d0f65c34201
tessam30 <tim.essam@gmail.com>
2022-10-26 13:37:10 +0200

    feat: create partner summary tables

--- Scripts/05_achv_tables.R
+++ Scripts/05_achv_tables.R
@@ -19,21 +19,17 @@
   library(patchwork)
   library(ggtext)
   library(selfdestructin5)
-  library(gt)
-  library(gtExtras)
   library(cascade) # Use dev version
   library(ggpattern)
   library(ggtext)
-  
+  library(gt)
+  library(selfdestructin5)
   
   # SI specific paths/functions  
   load_secrets()
   merdata <- file.path(glamr::si_path("path_msd"))
   file_path <- return_latest(folderpath = merdata,
                              pattern = "Genie-PSNUByIMs-Zambia")
-  
-  plhiv_path <- return_latest(folderpath = merdata,
-                              pattern = "SUBNAT")
 
   # REF ID for plots
   ref_id <- "98614ee3"  
@@ -43,205 +39,131 @@
   
   # Functions  
   source("Scripts/99_custom_functions.r")
+  swap_targets <- function(.data, mech1 = "18304", mech2 = "82075") {
+    # Using EQUIP as default as this has to be done each time in FY21
+    .data %>%
+      mutate(mech_code = ifelse(mech_code == {{mech1}}, {{mech2}}, mech_code),
+             mech_name = ifelse(mech_code == {{mech2}}, "Action HIV", mech_name))
+  }
   
 
 # MUNGE -------------------------------------------------------------------
 
   df <- read_msd(file_path) %>% 
     fix_mech_names() %>% 
-    clean_agency()
+    clean_agency() %>% 
+    swap_targets()
   
-  # Looking for a ACHV table by IP across the following
-  # indic_list <- c("PrEP_NEW", "HTS_TST_POS", "TX_CURR", "TX_NEW", "VLS", "VLC")
+  mdb_df   <- make_mdb_df(df)
+  mdb_tbl  <- reshape_mdb_df(mdb_df, metadata$curr_pd)  
   
-  df_achv <- df %>% 
-    filter(indicator %in% c("PrEP_NEW", "HTS_TST_POS", "TX_NEW", "TX_CURR"),
-           standardizeddisaggregate == "Total Numerator", 
-           fiscal_year == metadata$curr_fy, 
-           funding_agency != "DEDUP") %>% 
-    group_by(fiscal_year, mech_code, mech_name, funding_agency, indicator) %>% 
-    summarise(across(matches("cumul|targ"), sum, na.rm = T), .groups = "drop") %>% 
-    calc_achievement() %>% 
-    adorn_achievement() %>% 
-    mutate(tgt_rslt_sum = format_achv(targets, cumulative))
-           
-  # So, we want to create a similar shaped data frame so we can bind rows. 
-  # BUT -- VLS and VLC do not have "targets" so we'll use actual TX_PVLS and TX_CURR #s in place
+  # Create the treatment data frame needed for derived indicators
+  mdb_df_tx    <- make_mdb_tx_df(df)
+  mdb_tbl_tx   <- reshape_mdb_tx_df(mdb_df_tx, metadata$curr_pd)
   
-  df_vl <- df %>%
-    filter(fiscal_year == metadata$curr_fy, funding_agency != "DEDUP") %>% 
-    create_vl_df(mech_name, mech_code, funding_agency) %>% 
-    filter(period == metadata$curr_pd) %>% 
-    select(-vls_adj) %>% 
-    pivot_longer(cols = vlc:vls, 
-                 names_to = "indicator",
-                 values_to = "achievement") %>% 
-      mutate(tgt_rslt_sum = case_when(
-      indicator == "vls" ~ format_achv(tx_pvls, tx_pvls_d),
-      indicator == "vlc" ~ format_achv(tx_pvls_d, tx_curr_lag2)
-    )) %>% 
-    adorn_achievement() %>% 
-    mutate(indicator = str_to_upper(indicator),
-           fiscal_year = metadata$curr_fy, .after = "period") %>% 
-    select(-c(period, tx_curr, tx_curr_lag2, tx_pvls, tx_pvls_d))
+
+# CREATE TABLES -----------------------------------------------------------
+
+  mdb_tbl %>% 
+    # filter(indicator != "GEND_GBV") %>%
+    create_mdb(ou = "Zambia", type = "main", metadata$curr_pd, metadata$source) %>% 
+    gtsave(path = "Images", filename = glue::glue("Zambia_{metadata$curr_pd}_mdb_main.png"))  
   
-  # Bind'em together so we can plot them in a table
-  df_achv_all <- 
-    bind_rows(df_achv, df_vl) %>% 
-    mutate(indicator = fct_relevel(indicator, c("PrEP_NEW", "HTS_TST_POS", "TX_CURR", "TX_NEW", "VLS", "VLC"))) %>% 
-    arrange(mech_name, indicator)  
+  
+  create_mdb(mdb_tbl_tx, ou = "Zambia", type = "treatment", metadata$curr_pd, metadata$source) %>% 
+    bold_column(., Q4) %>% 
+    embiggen() %>% 
+    gtsave(., path = "Images", filename = glue::glue("{metadata$curr_pd}_Zambia_MMD_VL_MD.png"))    
+  
+
+# CREATE PARTNER TABLES ---------------------------------------------------
+
+  mk_ptr_tbl <- function(df, mech_id)  {
     
-# One Take  
-  df_achv_all %>% 
-    select(mech_code, mech_name, indicator, achievement, tgt_rslt_sum, achv_color) %>% 
-    mutate(achv = percent(achievement, 1), 
-           value = format_indicator(achv, tgt_rslt_sum, achv_color)) %>% 
-    select(mech_code, mech_name, indicator, value) %>% 
-    pivot_wider(names_from = "indicator",
-                values_from = "value") %>% 
-    filter(mech_code %ni% c("18327", "18304", "85117", 
-                            "17400", "17422", "18487",
-                            "17410", "160806", "18528")) %>% 
-    gt() %>% 
-    gt::fmt_markdown(columns = 3:8) %>% 
-    sub_missing(missing_text = "-") %>% 
-    cols_hide(mech_code)
-  
-# Standard table w/ colors
-  df_ach_all_gt <- df_achv_all %>% 
-    mutate(mech_name = str_replace_all(mech_name, "( Follow On| Follow on)", "")) %>% 
-    filter(mech_code %ni% c("18327", "18304", "85117", 
-                            "17400", "17422", "18487",
-                            "17410", "160806", "18528", "82054", "160789")) %>% 
-    select(mech_code, mech_name, indicator, achievement, funding_agency) %>% 
-    pivot_wider(names_from = "indicator",
-                values_from = "achievement")
-
-  
-
-
-# PEDS MUNGING ------------------------------------------------------------
- 
-   df_achv_peds <- df %>% 
-    filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "TX_NEW", "TX_CURR"),
-           standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Modality/Age/Sex/Result"), 
-           fiscal_year == metadata$curr_fy, 
-           funding_agency != "DEDUP", 
-           trendscoarse == "<15") %>% 
-    group_by(fiscal_year, mech_code, mech_name, funding_agency, indicator) %>% 
-    summarise(across(matches("cumul|targ"), sum, na.rm = T), .groups = "drop") %>% 
-    calc_achievement() %>% 
-    adorn_achievement() %>% 
-    mutate(tgt_rslt_sum = format_achv(targets, cumulative))
-
-
-  # BUT -- VLS and VLC do not have "targets" so we'll use actual TX_PVLS and TX_CURR #s in place
-  
-  df_vl_peds <- df %>%
-    filter(fiscal_year == metadata$curr_fy, funding_agency != "DEDUP",
-           trendscoarse == "<15") %>% 
-    create_vl_df(mech_name, mech_code, funding_agency, trendscoarse) %>% 
-    filter(period == metadata$curr_pd) %>% 
-    select(-vls_adj) %>% 
-    pivot_longer(cols = vlc:vls, 
-                 names_to = "indicator",
-                 values_to = "achievement") %>% 
-    mutate(tgt_rslt_sum = case_when(
-      indicator == "vls" ~ format_achv(tx_pvls, tx_pvls_d),
-      indicator == "vlc" ~ format_achv(tx_pvls_d, tx_curr_lag2)
-    )) %>% 
-    adorn_achievement() %>% 
-    mutate(indicator = str_to_upper(indicator),
-           fiscal_year = metadata$curr_fy, .after = "period") %>% 
-    select(-c(period, tx_curr, tx_curr_lag2, tx_pvls, tx_pvls_d))
-  
-  # Bind'em together so we can plot them in a table
-  df_achv_pedsl <- 
-    bind_rows(df_achv_peds, df_vl_peds) %>% 
-    mutate(indicator = fct_relevel(indicator, c("HTS_TST", "HTS_TST_POS", "TX_CURR", "TX_NEW", "VLS", "VLC"))) %>% 
-   filter(mech_code %ni% c("18327", "18304", "85117", 
-                                      "17400", "17422", "18487",
-                                      "17410", "160806", "18528", "82054", "160789")) %>% 
-    arrange(mech_name, indicator)  %>% 
-    select(mech_code, mech_name, indicator, achievement, funding_agency) %>% 
-    pivot_wider(names_from = "indicator",
-                values_from = "achievement")
-  
-# CREATE GT TABLES --------------------------------------------------------
-
-      
-  tmp <- df_achv_pedsl %>% 
-    gt(groupname_col = "funding_agency") %>% 
-    sub_missing(missing_text = "-") %>% 
-    fmt_percent(columns = 3:9, decimals = 0)
+    ip_mdb <- 
+      df %>% 
+      filter(mech_code == mech_id) %>% 
+      make_mdb_df() %>% 
+      reshape_mdb_df(., metadata$curr_pd) 
     
-  
-  indic_cols <- names(df_achv_pedsl)[4:9]
-  for(i in seq_along(indic_cols)){
-   tmp <- tmp %>% 
-     tab_style(
-      style = cell_fill(color = "#ff939a", alpha = 0.75),
-      locations = cells_body(
-        columns = indic_cols[i],
-        rows = tmp$`_data`[[indic_cols[i]]] < 0.75
-      )
-     ) %>% 
-     tab_style(
-       style = cell_fill(color = "#ffcaa2", alpha = 0.75),
-       locations = cells_body(
-         columns = indic_cols[i],
-         rows = tmp$`_data`[[indic_cols[i]]] >= 0.75 &  tmp$`_data`[[indic_cols[i]]] <= 0.89
-       )
-     ) %>% 
-     tab_style(
-       style = cell_fill(color = "#5BB5D5", alpha = 0.75),
-       locations = cells_body(
-         columns = indic_cols[i],
-         rows = tmp$`_data`[[indic_cols[i]]] >= 0.9 &  tmp$`_data`[[indic_cols[i]]] <= 1.1
-       )
-     ) %>% 
-     tab_style(
-       style = cell_fill(color = "#e6e6e6", alpha = 0.75),
-       locations = cells_body(
-         columns = indic_cols[i],
-         rows = tmp$`_data`[[indic_cols[i]]] > 1.1
-       )
-     ) %>% 
-     tab_style(
-       style = cell_borders(color = "#ffffff", weight = px(2)),
-       locations = cells_body(
-         columns = everything(),
-         rows = everything()
-       )
-     ) %>% 
-     cols_width(
-       mech_code ~ px(50),
-       everything() ~ px(100)
-     )
-  } 
+    mech_name <-  
+      df %>% 
+      filter(mech_code == mech_id) %>%
+      distinct(mech_name) %>% 
+      pull(mech_name)
     
-  tmp %>% 
-    tab_header(
-      title = glue("ZAMBIA MECHANISM PERFORMANCE SUMMARY PEDIATRICS"),
-      subtitle = legend_chunk
+    
+    ip_mdb %>%   
+      create_mdb(ou = "Zambia", type = "main", metadata$curr_pd, metadata$source) %>% 
+      tab_header(
+        title = glue::glue("{mech_name} PERFORMANCE SUMMARY")
+      ) %>% 
+      gtsave(path = "Images", filename = glue::glue("{mech_name}_mdb_main.png"))
+  }
+  
+  #mk_ptr_tbl(df, 82075) 
+  mk_ptr_tbl(df, 17413)
+  mk_ptr_tbl(df %>% 
+               mutate(mech_name = ifelse(str_detect(mech_name, "DISCOVER-H"), "DISCOVER-H", mech_name)), 17399)
+  
+
+
+# ZAM HEALTH --------------------------------------------------------------
+
+  # ADD CUSTOM TABLE FOR ZAMHEALTH ------------------------------------------
+  
+  
+  mk_ptr_tbl(df, 82086)
+  
+  
+  ip_mdb <- 
+    df %>% 
+    filter(mech_code == 82086, fiscal_year < 2023) %>% 
+    make_mdb_df() %>% 
+    filter(fiscal_year == metadata$curr_fy, operatingunit == "Global") %>% 
+    select(agency, indicator, indicator_plain, qtr1, qtr2, qtr3, qtr4, cumulative, targets) %>% 
+    mutate(achv = cumulative / targets) %>% 
+    mutate_if(is.numeric, list(~na_if(., Inf)))
+  
+  ip_mdb %>% 
+    gt(groupname_col ="agency") %>% 
+    gt_merge_stack(col1 = indicator, col2 = indicator_plain,
+                   palette = c(grey90k, grey80k),
+                   font_size = c("12px", "11px"),
+                   font_weight = c("normal", "normal")) %>% 
+    cols_align(columns = 2, 
+               align = c("left")) %>% 
+    cols_label(
+      indicator = ""
     ) %>% 
+    fmt_number(
+      columns = 4:9, 
+      decimals = 0
+    ) %>% 
+    fmt_percent(
+      columns = 10,
+      decimals = 0
+    ) %>% 
+    sub_missing(missing_text = "-") %>% 
+    tab_header(title = md("Zambia Accessible Markets for Health Performance Summary")) %>% 
     tab_source_note(
-      source_note = gt::md(glue::glue("{metadata$caption}"))
+      source_note = gt::md(glue::glue("**Source**: {authors_footnote(metadata$source)} | si.coreanalytics@usaid.gov"))
+    ) %>% 
+    gt::tab_style(
+      style = list("font-variant: small-caps;"),
+      locations = gt::cells_column_labels(columns = tidyselect::everything())
     ) %>% 
     tab_options(
-      source_notes.font.size = px(10)
+      source_notes.font.size = 8,
+      source_notes.padding = px(1),
+      data_row.padding = px(5),
+      table.font.size = 13
     ) %>% 
-    selfdestructin5::bold_rowgroup() %>% 
-    cols_label(mech_name = "", 
-               mech_code = "") %>% 
-    cols_hide(mech_code) %>% 
-    gtsave_extra("Images/USAID_summary_table_achv_ped.png")
-
-    
+    tab_spanner(
+      label = glue::glue("FY22 Summary"),
+      columns = 4:9
+    ) %>% 
+    gtsave(path = "Images", filename = glue::glue("ZamHealth_mdb_main.png"))
   
-  # Partners to show
-  # Action HIV, CIRKUITS, EPHO, Jhpiego, LPHO, SAFE, SPHO, UTH, SPHO, Zam-Health
   
-  # USAID Only partners -- SAFE, DISCOVER, ACTION HIV,
   
-  # Indicator order --> PrEP_NEW, HTS_TST_POS, TX_CURR, TX_NEW, VLS, VLC
